f3d5bfe74f434b0d1bbb819d6f48679c55de7ee4
Abdur Rehman Aqil <aqilabdur@gmail.com>
2023-10-04 00:38:05 -0400

    pieces class

---
+++ ChessAssignment/src/chess/Pieces.java
@@ -0,0 +1,32 @@
+package chess;
+
+public class Pieces {
+    
+    public class Bishop {
+
+    }
+
+    public class King {
+
+    }
+
+    public class Knight {
+
+    }
+
+    public class Pawn {
+
+    }
+
+    public class Piece {
+
+    }
+
+    public class Queen {
+
+    }
+
+    public class Rook {
+        
+    }
+}
