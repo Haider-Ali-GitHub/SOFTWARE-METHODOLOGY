fb84270eaed95c8acade0274c037834b215a2487
Haider Ali <haider.ali07304@gmail.com>
2023-10-11 22:37:48 -0400

    Daddy

--- ChessAssignment/src/chess/Chess.java
+++ ChessAssignment/src/chess/Chess.java
@@ -160,20 +160,14 @@ public class Chess {
 	 * This method should reset the game, and start from scratch.
 	 */
 	public static void start() {
-		/* FILL IN THIS METHOD */
-		// ArrayList<ReturnPiece> board = new ArrayList<>();
-	
-		// If board is not null, clear it to ensure that we're starting fresh
+		// Initialize or clear the board
 		if (board != null) {
 			board.clear();
 		} else {
-			// Initialize the board ArrayList
 			board = new ArrayList<>();
 		}
 
-		//Pawns 
-		for (PieceFile file : PieceFile.values())
-		{
+		for (PieceFile file : PieceFile.values()) {
 			ReturnPiece whitePawn = new ReturnPiece();
 			whitePawn.pieceFile = file;
 			whitePawn.pieceRank = 2;
@@ -185,125 +179,28 @@ public class Chess {
 			blackPawn.pieceRank = 7;
 			blackPawn.pieceType = PieceType.BP;
 			board.add(blackPawn);
-		}	
+		}
 
-		//ROOKS 
-		// Initialize white rooks
-		ReturnPiece whiteRook1 = new ReturnPiece();
-		whiteRook1.pieceFile = PieceFile.a;
-		whiteRook1.pieceRank = 1;
-		whiteRook1.pieceType = PieceType.WR;
-		board.add(whiteRook1);
-
-		ReturnPiece whiteRook2 = new ReturnPiece();
-		whiteRook2.pieceFile = PieceFile.h;
-		whiteRook2.pieceRank = 1;
-		whiteRook2.pieceType = PieceType.WR;
-		board.add(whiteRook2);
-
-		
-		// Initialize black rooks
-		ReturnPiece blackRook1 = new ReturnPiece();
-		blackRook1.pieceFile = PieceFile.a;
-		blackRook1.pieceRank = 8;
-		blackRook1.pieceType = PieceType.BR;
-		board.add(blackRook1);
-
-		ReturnPiece blackRook2 = new ReturnPiece();
-		blackRook2.pieceFile = PieceFile.h;
-		blackRook2.pieceRank = 8;
-		blackRook2.pieceType = PieceType.BR;
-		board.add(blackRook2);
-
-
-		//Knights 
-		// Initialize white knights
-		ReturnPiece whiteKnight1 = new ReturnPiece();
-		whiteKnight1.pieceFile = PieceFile.b;
-		whiteKnight1.pieceRank = 1;
-		whiteKnight1.pieceType = PieceType.WN;
-		board.add(whiteKnight1);
-
-		ReturnPiece whiteKnight2 = new ReturnPiece();
-		whiteKnight2.pieceFile = PieceFile.g;
-		whiteKnight2.pieceRank = 1;
-		whiteKnight2.pieceType = PieceType.WN;
-		board.add(whiteKnight2);
-
-		// Initialize black knights
-		ReturnPiece blackKnight1 = new ReturnPiece();
-		blackKnight1.pieceFile = PieceFile.b;
-		blackKnight1.pieceRank = 8;
-		blackKnight1.pieceType = PieceType.BN;
-		board.add(blackKnight1);
-
-		ReturnPiece blackKnight2 = new ReturnPiece();
-		blackKnight2.pieceFile = PieceFile.g;
-		blackKnight2.pieceRank = 8;
-		blackKnight2.pieceType = PieceType.BN;
-		board.add(blackKnight2);
-
-		//Bishop 
-		// Initialize white bishops
-		ReturnPiece whiteBishop1 = new ReturnPiece();
-		whiteBishop1.pieceFile = PieceFile.c;
-		whiteBishop1.pieceRank = 1;
-		whiteBishop1.pieceType = PieceType.WB;
-		board.add(whiteBishop1);
-
-		ReturnPiece whiteBishop2 = new ReturnPiece();
-		whiteBishop2.pieceFile = PieceFile.f;
-		whiteBishop2.pieceRank = 1;
-		whiteBishop2.pieceType = PieceType.WB;
-		board.add(whiteBishop2);
-
-		// Initialize black bishops
-		ReturnPiece blackBishop1 = new ReturnPiece();
-		blackBishop1.pieceFile = PieceFile.c;
-		blackBishop1.pieceRank = 8;
-		blackBishop1.pieceType = PieceType.BB;
-		board.add(blackBishop1);
-
-		ReturnPiece blackBishop2 = new ReturnPiece();
-		blackBishop2.pieceFile = PieceFile.f;
-		blackBishop2.pieceRank = 8;
-		blackBishop2.pieceType = PieceType.BB;
-		board.add(blackBishop2);
-
-
-		//Queens 
-		// Initialize white queen
-		ReturnPiece whiteQueen = new ReturnPiece();
-		whiteQueen.pieceFile = PieceFile.d;
-		whiteQueen.pieceRank = 1;
-		whiteQueen.pieceType = PieceType.WQ;
-		board.add(whiteQueen);
-
-		// Initialize black queen
-		ReturnPiece blackQueen = new ReturnPiece();
-		blackQueen.pieceFile = PieceFile.d;
-		blackQueen.pieceRank = 8;
-		blackQueen.pieceType = PieceType.BQ;
-		board.add(blackQueen);
-
-		//Kings 
-		// Initialize white king
-		ReturnPiece whiteKing = new ReturnPiece();
-		whiteKing.pieceFile = PieceFile.e;
-		whiteKing.pieceRank = 1;
-		whiteKing.pieceType = PieceType.WK;
-		board.add(whiteKing);
-
-		// Initialize black king
-		ReturnPiece blackKing = new ReturnPiece();
-		blackKing.pieceFile = PieceFile.e;
-		blackKing.pieceRank = 8;
-		blackKing.pieceType = PieceType.BK;
-		board.add(blackKing);
-		
-
-		// Set the current player to white
-		currentPlayer  = Player.white;	
+		PieceType[] pieceTypes = 
+		{PieceType.WR, PieceType.WN, PieceType.WB, PieceType.WQ, PieceType.WK, PieceType.WB, PieceType.WN, PieceType.WR};
+		PieceType[] blackPieceTypes = 
+		{PieceType.BR, PieceType.BN, PieceType.BB, PieceType.BQ, PieceType.BK, PieceType.BB, PieceType.BN, PieceType.BR};
+	
+		for (int i = 0; i < pieceTypes.length; i++) {
+			ReturnPiece whitePiece = new ReturnPiece();
+			whitePiece.pieceFile = PieceFile.values()[i];
+			whitePiece.pieceRank = 1;
+			whitePiece.pieceType = pieceTypes[i];
+			board.add(whitePiece);
+	
+			ReturnPiece blackPiece = new ReturnPiece();
+			blackPiece.pieceFile = PieceFile.values()[i];
+			blackPiece.pieceRank = 8;
+			blackPiece.pieceType = blackPieceTypes[i];
+			board.add(blackPiece);
+		}
+		currentPlayer = Player.white;
 	}
+	
 
 }
