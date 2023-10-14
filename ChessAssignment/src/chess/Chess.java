f083ab9cb562f82e337c42284af8eec0faa836d9
Haider Ali <haider.ali07304@gmail.com>
2023-10-14 19:19:16 -0400

    updated Pawn Promotion

--- ChessAssignment/src/chess/Chess.java
+++ ChessAssignment/src/chess/Chess.java
@@ -117,58 +117,45 @@ public class Chess {
 		}
 
 		        // Add Pawn Promotion logic
-				if(movingPiece.pieceType == PieceType.WP && destRank == 8 || 
-				movingPiece.pieceType == PieceType.BP && destRank == 1) {
-	 
-				 if(parts.length >= 3) { // Check if a promotion piece type is provided
-					 try {
-						 String promoTypeString = parts[2].toUpperCase();
-						 if(promoTypeString.length() == 1 && 
-							"RNBQ".contains(promoTypeString)) {
-							 
-							 PieceType promoType = PieceType.valueOf(
-								 (movingPiece.pieceType == PieceType.WP ? "W" : "B") + 
-								  promoTypeString);
-							 
+	if(movingPiece.pieceType == PieceType.WP && destRank == 8 || 
+		movingPiece.pieceType == PieceType.BP && destRank == 1) {
+	 	if(parts.length >= 3) { // Check if a promotion piece type is provided
+		 	try {
+					String promoTypeString = parts[2].toUpperCase();
+					if(promoTypeString.length() == 1 && "RNBQ".contains(promoTypeString)) {
+						PieceType promoType = PieceType.valueOf(
+							(movingPiece.pieceType == PieceType.WP ? "W" : "B") + promoTypeString);
 							 movingPiece.pieceType = promoType;
-						 } else {
+					}else {
 							 board.message = ReturnPlay.Message.ILLEGAL_MOVE;
 							 return board;
-						 }
-					 } catch(IllegalArgumentException e) {
-						 board.message = ReturnPlay.Message.ILLEGAL_MOVE;
-						 return board;
-					 }
-				 } else {
-					 // Default to Queen if no promotion type is provided
-					 movingPiece.pieceType = movingPiece.pieceType == PieceType.WP ? 
-											 PieceType.WQ : PieceType.BQ;
-				 }
-			 }
-	
+					}
+				} 	catch(IllegalArgumentException e) {
+				  		board.message = ReturnPlay.Message.ILLEGAL_MOVE;
+						return board;
+					}
+		} else {
+				// Default to Queen if no promotion type is provided
+				movingPiece.pieceType = movingPiece.pieceType == PieceType.WP ? PieceType.WQ : PieceType.BQ;
+			}
+		}
 		// Update movingPiece's position
 		movingPiece.pieceFile = destFile;
 		movingPiece.pieceRank = destRank;
 
-		
-
 		if (move.endsWith(" draw?"))  {
 		board.message = ReturnPlay.Message.DRAW;
 		return board;
-	}
+		}
 	
 	} else {
 		board.message = ReturnPlay.Message.ILLEGAL_MOVE;
 		return board;
-	}
-	
+		}
     // Switch the current player
     currentPlayer = (currentPlayer == Player.white) ? Player.black : Player.white;	
     return board;
 }
-
-
-
 
 public static ReturnPiece getPieceAt(PieceFile file, int rank) {
     for (ReturnPiece piece : board.piecesOnBoard) {
