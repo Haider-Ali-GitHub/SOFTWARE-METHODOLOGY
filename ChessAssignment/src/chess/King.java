e4ff3e67de5bead83f4ead29dc8fbd3dbf3594b3
Abdur Rehman Aqil <aqilabdur@gmail.com>
2023-10-15 17:56:21 -0400

    updated canCastle

--- ChessAssignment/src/chess/King.java
+++ ChessAssignment/src/chess/King.java
@@ -1,6 +1,8 @@
 package chess;
 import java.util.List;
 
+import chess.Chess.Player;
+
 public class King extends ReturnPiece {
     private boolean hasMoved;
 
@@ -23,24 +25,28 @@ public class King extends ReturnPiece {
 
         // Ensure the move is only one square in any direction
         if (Math.abs(fileDifference) <= 1 && Math.abs(rankDifference) <= 1) {
+            hasMoved = true;
             return true;
         }
 
         return false;
     }
     
-    public boolean canCastle(PieceFile desFile, int destRank, List<ReturnPiece> piecesOnBoard) {
-        if (this.pieceType != PieceType.WK && this.pieceType != PieceType.BK) {
+    public boolean canCastle(Player currentPlayer, PieceFile destFile, int destRank, List<ReturnPiece> piecesOnBoard) {
+        //
+
+        // check if the king is not in check
+        if (Chess.isInCheck(currentPlayer)) {
             return false;
         }
 
-        // Check if king has movedbefore   
+        // Check if theking has moved before
         if (this.hasMoved) {
             return false;
         }
 
         // check if the dest square is 2 squares away left or right
-        int fileDifference = desFile.ordinal() - this.pieceFile.ordinal();
+        int fileDifference = destFile.ordinal() - this.pieceFile.ordinal();
         if (Math.abs(fileDifference) != 2) {
             return false;
         }
@@ -50,18 +56,12 @@ public class King extends ReturnPiece {
             return false;
         }
 
-        // check if the squares between the king and rook are empty
-        int direction = (fileDifference > 0) ? 1 : -1;
-        int currentFile = this.pieceFile.ordinal() + direction;
-        while (currentFile != desFile.ordinal()) {
-            for (ReturnPiece piece : piecesOnBoard) {
-                if (piece.pieceFile.ordinal() == currentFile && piece.pieceRank == destRank) {
-                    return false;
-                }
-            }
-            currentFile += direction;
-        }
+        // check if the squares between the king and rook are emtpy
 
-        return true; 
+        
+
+
+
+        return true;
     }
 }
